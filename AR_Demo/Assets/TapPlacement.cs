ed48232627552c5ee3922782a60b8ce6ca266c5b
WesMcW <wesmcwhorter@gmail.com>
2019-10-28 12:01:49 -0500

    the thing

---
+++ AR_Demo/Assets/TapPlacement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+//using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
+using System;
+
+public class TapPlacement : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+
+    //private ARSessionOrigin arOrigin;
+
+    public Text test;
+    
+    private ARRaycastManager arRay;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        //arOrigin = FindObjectOfType<ARSessionOrigin>();
+        arRay = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdateIndicator();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        //arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
+        arRay.Raycast(screenCenter, hits, TrackableType.All);
+
+        placementPoseIsValid = hits.Count > 0;
+        if(placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+
+        test.text = hits.Count.ToString();
+    }
+
+    private void UpdateIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+}
