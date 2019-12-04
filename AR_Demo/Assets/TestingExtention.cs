0b375a0606dcde2f8d5e5a22a97767946ecb56d4
Lebrra <Lebrra@users.noreply.github.com>
2019-12-04 14:51:14 -0600

    code working better

---
+++ AR_Demo/Assets/TestingExtention.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingExtention : DefaultTrackableEventHandler
+{
+    public bool isOn = false;
+    public GameObject cube;
+
+    protected override void OnTrackingFound()
+    {
+        if (mTrackableBehaviour)
+        {
+            isOn = true;
+
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Enable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = true;
+
+            // Enable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = true;
+
+            // Enable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isOn && !cube.activeSelf) cube.SetActive(true);
+    }
+}
