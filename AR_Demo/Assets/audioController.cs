c1876e45576f5e97d473bbf5158acaf8b25e833b
ChrisRMc <60404889+ChrisRMc@users.noreply.github.com>
2020-01-28 15:13:50 -0600

    Audio Script Begining
    Created basic Audio Script and started creating methods and references in the script.

--- AR_Demo/Assets/audioController.cs
+++ AR_Demo/Assets/audioController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class audioController : MonoBehaviour
 {
+    public AudioSource clip;
+    GameObject willy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        clip = GetComponent<AudioSource>();
+        playWillyAudio();
+        willy = GameObject.Find("WillyW"); //finds game object with specified name
+   
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    //call to play clip
+    public void playWillyAudio()
+    {
+        if (!clip.isPlaying)
+            clip.Play();
+    }
+    //plays clip when object is enabled
+    private void OnEnable()
+    {
+        playWillyAudio();
+    }
+    private void OnDisable()
+    {
+        clip.Stop();
     }
 }
