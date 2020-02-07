fe8cd9bbff40ff8fe9239998c3918bc454bd38c3
Leah <Lebrra@users.noreply.github.com>
2020-02-06 20:20:11 -0600

    maybe progress

--- AR_Demo/Assets/TrackingFound.cs
+++ AR_Demo/Assets/TrackingFound.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class TrackingFound : MonoBehaviour
 {
+    public GameObject disableImageTrack;
     public GameObject enablePlace;
     public GameObject enableCanvas;
+    public GameObject startAudio;
+    public GameObject willie;
 
     private void Start()
     {
         enablePlace.SetActive(false);
+        startAudio.SetActive(false);
     }
 
     public void TrackingFoundMethod()
     {
         enableCanvas.SetActive(true);
         enablePlace.SetActive(true);
+        disableImageTrack.SetActive(false);
     }
 
     public void OnObjectPlaced()
     {
         enablePlace.SetActive(false);
         enableCanvas.SetActive(false);
+        startAudio.SetActive(true);
+        willie.GetComponent<Animator>().SetTrigger("Start");
+        // start willie animation
+        // when animation ends, reset things
     }
 }
---
+++ AR_Demo/Assets/reset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reset : MonoBehaviour
+{
+    public GameObject resetAudio;
+    public GameObject resetTracking;
+
+    public void resetApp()
+    {
+        resetAudio.SetActive(false);
+        resetTracking.SetActive(true);
+    }
+}
